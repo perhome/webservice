81a9795bbdcf3dc6d9c250226b5b126e31914fa4
wxg4dev <wxg4dev@gmail.com>
2014-10-23 20:53:18 +0800

    补充logs与applcaitions

---
+++ applications/kohana.php
@@ -0,0 +1,15 @@
+<?php
+
+define('MODPATH', '/home/webservice/applications/kohana/modules/');
+define('SYSPATH', '/home/webservice/applications/kohana/system/');
+
+define('STATIC_HOME_URL', 'http://data.perhome.cn/');
+define('STATIC_PUHUA_URL', 'http://data.puhua.co/');
+
+define ('SCWS_TEST_MULTI_SHORT',  0x01);
+define ('SCWS_TEST_MULTI_DUALITY',  0x02);
+define ('SCWS_TEST_MULTI_ZMAIN',  0x04);
+define ('SCWS_TEST_MULTI_ZALL',   0x08);
+define ('SCWS_TEST_MULTI_MASK',   0x0f);
+define ('SCWS_TEST_IGNORE_SYMBOL',  0x10);
+define ('SCWS_TEST_DUALITY',    0x20);
